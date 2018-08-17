38bc4a4d640575252e9e18ba58a7c18107172842
Reaganhatt <reagan.hatt@gmail.com>
2018-08-17 14:00:18 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,19 @@
 class ApplicationController < Sinatra::Base
 
-  configure do
-    set :public_folder, 'public'
-    set :views, 'app/views'
+get '/' do
+  erb :index
+end
+
+get '/results' do
+  erb :results
+end
+
+
+
+
+ configure do
+   set :public_folder, 'public'
+   set :views, 'app/views'
   end
 
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,12 @@
-class Question
+#this file should contain ruby and doesn not have to be a class - could be a method or other ruby code
+class ApplicationController < Sinatra::Base
+
+configure do
+  set :public_folder, 'public'
+  set :views, 'app/views'
+
+end
+#this file should contain my get routes (what I learned Tuesday with Siantra!)
+
 
 end
